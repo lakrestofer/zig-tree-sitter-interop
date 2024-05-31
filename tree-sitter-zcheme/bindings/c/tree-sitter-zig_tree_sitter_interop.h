5f1716c88aaa3ff2819605b2098a991895b7147d
lakrestofer <lakrestofer@gmail.com>
2024-05-31 14:50:03 +0200

    generate parser

---
+++ tree-sitter-zcheme/bindings/c/tree-sitter-zig_tree_sitter_interop.h
@@ -0,0 +1,16 @@
+#ifndef TREE_SITTER_ZIG_TREE_SITTER_INTEROP_H_
+#define TREE_SITTER_ZIG_TREE_SITTER_INTEROP_H_
+
+typedef struct TSLanguage TSLanguage;
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+const TSLanguage *tree_sitter_zig_tree_sitter_interop(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // TREE_SITTER_ZIG_TREE_SITTER_INTEROP_H_
